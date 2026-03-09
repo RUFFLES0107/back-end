264e6d35a6c5b990f05da4de7ba74a7326ab388e
RUFFLES <rafael.lohmann@escola.pr.gov.br>
2026-03-09 08:57:04 -0300

    Add files via upload

---
+++ pasta do ruffles/aula1.js
@@ -0,0 +1,15 @@
+let nome = "Rafael"
+let altura = 1.72
+let idade = 15
+let genero = "homem" 
+let cor = "banca" 
+let cabelo = "loiro escuro liso"
+let acessorio = "aneis, um preto e um prata"
+let roupa = "uniforme" 
+let cenario = "laboratorio da escola" 
+
+
+
+
+console.log( "ola, eu sou o " + nome + ", sou um " + genero + ", de pele " + cor + ", e de cabelo " + cabelo + ", estou usando " + acessorio + ", estou vestindo o " + roupa + ", e estou no " + cenario + ".")
+
